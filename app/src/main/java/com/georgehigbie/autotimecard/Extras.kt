72c4d72fefbc0ec42a8c35f2e427d42c1e874da8
George Higbie <gehigbie@gmail.com>
2017-11-04 20:27:08 -0600

    Add location variable and shared prefs

---
+++ app/src/main/java/com/georgehigbie/autotimecard/Extras.kt
@@ -0,0 +1,8 @@
+package com.georgehigbie.autotimecard
+
+/**
+ * Created by georgehigbie on 11/4/17.
+ */
+val EXTRA_LOCATION: String = "extraLocation"
+val SHARED_PREFS_FILENAME: String = "appSettings"
+val LOCATION_SET: String = "locationSet"
--- app/src/main/java/com/georgehigbie/autotimecard/MainActivity.kt
+++ app/src/main/java/com/georgehigbie/autotimecard/MainActivity.kt
@@ -1,14 +1,18 @@
 package com.georgehigbie.autotimecard
 
 import android.content.Context
+import android.content.SharedPreferences
 import android.location.LocationManager
 import android.os.Bundle
+import android.preference.PreferenceManager
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     private var locationManager: LocationManager? = null
+    private var appSettings: SharedPreferences? = null
+    private var locationSet: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,16 +20,28 @@ class MainActivity : AppCompatActivity() {
 
         setButtonVisibility()
         setOnClickListeners()
+
     }
 
     fun setButtonVisibility(){ //this should set the visibility of a button if location is set
+        if(locationSet == false){
 
+        }
     }
 
 
     fun getLocation() { //This should be called by setLocationButton and changeLocationButton
         if(locationManager == null) {
             locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager?
+
+            var locationSet: Boolean = true
+            appSettings = this.getSharedPreferences(com.georgehigbie.autotimecard.SHARED_PREFS_FILENAME, )
+
+            val sharedPreferences: SharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
+            val editor: SharedPreferences.Editor = sharedPreferences.edit()
+            editor.putBoolean(LOCATION_SET, locationSet)
+
+
 
 
         }
@@ -34,6 +50,7 @@ class MainActivity : AppCompatActivity() {
     fun setOnClickListeners(){
         setLocationButton.setOnClickListener {
             getLocation();
+
         }
 
         changeLocationButton.setOnClickListener {
