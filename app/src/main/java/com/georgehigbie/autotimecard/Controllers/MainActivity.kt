fcffca746d9cdbe723755f12972d65ba1194984d
George Higbie <gehigbie@gmail.com>
2017-11-04 20:52:36 -0600

    Add intent to button

--- app/src/main/java/com/georgehigbie/autotimecard/Controllers/MainActivity.kt
+++ app/src/main/java/com/georgehigbie/autotimecard/Controllers/MainActivity.kt
@@ -1,12 +1,15 @@
-package com.georgehigbie.autotimecard
+package com.georgehigbie.autotimecard.Controllers
 
 import android.content.Context
+import android.content.Intent
 import android.content.SharedPreferences
 import android.location.LocationManager
 import android.os.Bundle
 import android.preference.PreferenceManager
 import android.support.v7.app.AppCompatActivity
 import android.view.View
+import com.georgehigbie.autotimecard.LOCATION_SET
+import com.georgehigbie.autotimecard.R
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -21,15 +24,13 @@ class MainActivity : AppCompatActivity() {
 
         setButtonVisibility()
         setOnClickListeners()
-
     }
 
     /*** This sets the visibility of the buttons if location is set or not set ***/
     fun setButtonVisibility(){ //sets the visibility of a button if location is set
         if(locationSet == false){
             setLocationButton.visibility = View.VISIBLE
-        }else{
-            changeLocationButton.visibility = View.VISIBLE
+            changeLocationButton.visibility = View.GONE
         }
     }
 
@@ -43,9 +44,6 @@ class MainActivity : AppCompatActivity() {
             val editor: SharedPreferences.Editor = sharedPreferences.edit()
             editor.putBoolean(LOCATION_SET, locationSet)
 
-
-
-
         }
     }
 
@@ -57,6 +55,11 @@ class MainActivity : AppCompatActivity() {
 
         changeLocationButton.setOnClickListener {
 
+        }
+
+        seeTimeCardButton.setOnClickListener {
+            val timeCardIntent = Intent(this, TimeCard::class.java)
+            startActivity(timeCardIntent)
         }
     }
 }
