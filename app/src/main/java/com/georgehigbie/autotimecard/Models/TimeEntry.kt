8e6dc9713713d7cd453b399e249e8d55643598d9
George Higbie <gehigbie@gmail.com>
2017-11-06 22:27:39 -0700

    Add grid layout

--- app/src/main/java/com/georgehigbie/autotimecard/Models/TimeEntry.kt
+++ app/src/main/java/com/georgehigbie/autotimecard/Models/TimeEntry.kt
@@ -1,9 +1,17 @@
 package com.georgehigbie.autotimecard.Models
 
+import java.text.SimpleDateFormat
+import java.util.*
+
 /**
  * Created by georgehigbie on 11/4/17.
  */
 class TimeEntry {
 
+    var date:Date = Date()
+    var dayOfWeek: SimpleDateFormat = SimpleDateFormat("EEEE")
+    var dateReadable: SimpleDateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+    var timeReadable: SimpleDateFormat = SimpleDateFormat("HH:mm")
+    //var clockIn: Boolean
 
 }
