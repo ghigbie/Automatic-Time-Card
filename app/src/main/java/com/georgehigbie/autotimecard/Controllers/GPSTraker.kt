e47fec60fd4e12ffea5297edeabe68048053b33c
George Higbie <gehigbie@gmail.com>
2017-11-10 22:13:18 -0700

    Add permission to manifest file

--- app/src/main/java/com/georgehigbie/autotimecard/Controllers/GPSTraker.kt
+++ app/src/main/java/com/georgehigbie/autotimecard/Controllers/GPSTraker.kt
@@ -5,6 +5,7 @@ import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
 import android.os.Bundle
+import android.util.Log
 
 /**
  * Created by georgehigbie on 11/10/17.
@@ -21,6 +22,9 @@ class GPSTraker(mmContext: Context) : LocationListener{
     protected var locationManager: LocationManager? = null
     private var mContext:Context = mmContext
 
+    private val MIN_DISTANCE_CHANGE_FOR_UPDATES: Long = 100 // 100 meters
+    private val MIN_TIME_BW_UPDATES = (1000 * 60 * 1).toLong() // 60 seconds
+
     init {
         val m_location: Location? = getLocation()
     }
@@ -29,9 +33,31 @@ class GPSTraker(mmContext: Context) : LocationListener{
         try {
             locationManager = mContext.getSystemService(Context.LOCATION_SERVICE) as LocationManager
             isGPSEnabled = locationManager!!.isProviderEnabled(LocationManager.GPS_PROVIDER)
-            println("****isGPSEnabled****")
+            println("**** isGPSEnabled ****")
             isNetworkEnabled = locationManager!!.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
-            println("***isNetworkEnabled****")
+            println("**** isNetworkEnabled ****")
+            if(isGPSEnabled && !isNetworkEnabled) {
+                //no network provider is Enabled
+                println("**** No Network Provider is Enabled ****")
+            }else{
+                this.canGetLocation = true
+                if(isGPSEnabled){
+                    if(location == null){
+                        locationManager!!.requestLocationUpdates(
+                                LocationManager.GPS_PROVIDER,
+                                MIN_TIME_BW_UPDATES,
+                                MIN_DISTANCE_CHANGE_FOR_UPDATES.toFloat(),
+                                this)
+                        Log.d("GPS", "GPS Enabled")
+                    }
+                    if(locationManager != null){
+                         location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+                    }
+                }
+                }
+            }
+
+
 
         }catch(e: Exception){
             e.printStackTrace()
@@ -65,10 +91,10 @@ class GPSTraker(mmContext: Context) : LocationListener{
     override fun onProviderEnabled(provider: String?) {}
     override fun onProviderDisabled(provider: String?) {}
 
-    companion object{
-        private val MIN_DISTANCE_CHANGE_FOR_UPDATES: Long = 100 // 100 meters
-        private val MIN_TIME_BW_UPDATES = (1000 * 60 * 1).toLong() // 60 seconds
-    }
+//    companion object{
+//        private val MIN_DISTANCE_CHANGE_FOR_UPDATES: Long = 100 // 100 meters
+//        private val MIN_TIME_BW_UPDATES = (1000 * 60 * 1).toLong() // 60 seconds
+//    }
 
 
 }
