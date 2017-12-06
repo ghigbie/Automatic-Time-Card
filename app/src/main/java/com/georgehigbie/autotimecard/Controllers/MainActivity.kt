e0a8dc52d2c75bc82a221e9be9778620646a5164
George Higbie <gehigbie@gmail.com>
2017-12-05 21:20:36 -0700

    Change activity type in inherit from permision activity

--- app/src/main/java/com/georgehigbie/autotimecard/Controllers/MainActivity.kt
+++ app/src/main/java/com/georgehigbie/autotimecard/Controllers/MainActivity.kt
@@ -1,28 +1,29 @@
 package com.georgehigbie.autotimecard.Controllers
 
-import android.annotation.SuppressLint
-import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
 import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.widget.TextView
 import com.georgehigbie.autotimecard.R
+import io.vrinda.kotlinpermissions.PermissionsActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity(){
+class MainActivity : PermissionsActivity(){
 
+   // private lateinit var fusedLocationClient: FusedLocationProviderClient
     private var locationManager: LocationManager? = null
+
     private val locationListener: LocationListener? = null
     private var appSettings: SharedPreferences? = null
     private var locationSet: Boolean = false
     private var location: Location? = null
     private var testingLongText: TextView? = null
     private var testingLatText: TextView? = null
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -43,36 +44,36 @@ class MainActivity : AppCompatActivity(){
         }
     }
 
+//
+//    @SuppressLint("MissingPermission")
+//    fun getLocation() { //This should be called by setLocationButton and changeLocationButton
+//        locationManager = this.getSystemService(Context.LOCATION_SERVICE) as LocationManager?
+//        val locationListener = object : LocationListener{
+//            override fun onLocationChanged(location: Location?) {
+//                var longitudeString: String = (location!!.longitude).toString()
+//                var latitudeString: String = (location!!.latitude).toString()
+//                testingLongText!!.text = "Longitude: $longitudeString"
+//                testingLatText!!.text = "Latitude: $latitudeString"
+//            }
+//
+//            override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {}
+//            override fun onProviderEnabled(provider: String?) {}
+//            override fun onProviderDisabled(provider: String?) {}
+//        }
+//        locationManager?.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0L, 0f, locationListener) //safe check supression
+//
+//    }
 
-    @SuppressLint("MissingPermission")
-    fun getLocation() { //This should be called by setLocationButton and changeLocationButton
-        locationManager = this.getSystemService(Context.LOCATION_SERVICE) as LocationManager?
-        val locationListener = object : LocationListener{
-            override fun onLocationChanged(location: Location?) {
-                var longitudeString: String = (location!!.longitude).toString()
-                var latitudeString: String = (location!!.latitude).toString()
-                testingLongText!!.text = "Longitude: $longitudeString"
-                testingLatText!!.text = "Latitude: $latitudeString"
-            }
-
-            override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {}
-            override fun onProviderEnabled(provider: String?) {}
-            override fun onProviderDisabled(provider: String?) {}
-        }
-        locationManager?.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0L, 0f, locationListener) //safe check supression
-
-    }
-
-
-
-    fun getLocationUpdates(){
+    fun getLocation(){
 
     }
 
+
+
+    //sets all onclick listeners for activity
     fun setOnClickListeners(){
         setLocationButton.setOnClickListener {
             getLocation()
-
         }
 
         changeLocationButton.setOnClickListener {
